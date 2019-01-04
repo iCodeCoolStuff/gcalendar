4f2cc442ea8d4cdb6f2a11aa4f9d1e6bb5032ecf
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2019-01-04 10:19:19 -0500

    added save function but it's untested

--- gcalendar.py
+++ gcalendar.py
@@ -11,7 +11,19 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
+#Start library
+
 DATE_PATTERN = re.compile(r'(\d{4}).(\d{2}).(\d{2})')
+
+WEEKDAYS = {
+    'sunday':    0, 
+    'monday':    1, 
+    'tuesday':   2, 
+    'wednesday': 3, 
+    'thursday':  4, 
+    'friday':    5, 
+    'saturday':  6,
+} 
 
 def RFC_from_dt(date, time):
     return date + 'T' + time + 'Z'
@@ -59,11 +71,9 @@ def dt_to_gmt(dt):
     else:
         return dt
 
-def get_events(service):
-    today = datetime.datetime.today() 
-
-    mintime = RFC_from_UTC(dt_to_gmt(get_min_today()))
-    maxtime = RFC_from_UTC(dt_to_gmt(get_max_today()))
+def get_events(service, dt):
+    mintime = RFC_from_UTC(dt_to_gmt(dt))
+    maxtime = RFC_from_UTC(dt_to_gmt(dt))
 
     result = service.events().list(calendarId='primary', timeMin=mintime, timeMax=maxtime,
                                     singleEvents=True, orderBy='startTime').execute()
@@ -72,17 +82,40 @@ def get_events(service):
         return None
     return items
 
-def get_days_of_week():
+def get_days_of_week(dt):
+    date = calendar.weekday(dt.year, dt.month, dt.day)
+    if date == 6:
+        days = []
+        for i in range(6):
+            days.append(datetime.datetime(dt.year, dt.month, dt.day+i))
+        return days
 
+    offset = 0
+    while date != 6:
+        offset -= 1 
+        date -= 1
+    days = []
+    for i in range(6):
+        days.append(datetime.datetime(dt.year, dt.month, dt.day+(i+offset)))
+    return days
+
+def dt_from_date(date):
+    dt = datetime.datetime(
+            date.group(1),
+            date.group(2), 
+            date.group(3))
+    return dt
 
 def save_events(events, filename):
-    with open(filename, 'w') as f:
+    with open(filename + '.json', 'w') as f:
         json.dump(events, f)
 
 def load_events(filename):
-    with open(filename, 'r') as f:
+    with open(filename + '.json', 'r') as f:
         items = json.load(f)
         return items
+
+# End library
 
 @click.group()
 @click.pass_context
@@ -94,15 +127,29 @@ def cli(ctx):
         creds = tools.run_flow(flow, store)
     service = build('calendar', 'v3', http=creds.authorize(Http()))
 
-    ctx.obj = service
+    ctx.ensure_object(dict)
+    ctx.obj['service'] = service
+    ctx.obj['week']    = get_days_of_week(datetime.datetime.today()) 
 
 @cli.command()
 @click.option('--day', type=str, default='today')
 @click.argument('schedule_name', type=str)
 @click.pass_context
 def save(ctx, schedule_name, day):
-    events = get_current_events(ctx.obj)
-    
+    if day == 'today':
+        events = get_events(ctx.obj, datetime.datetime.today())
+    elif day in WEEKDAYS.keys():
+        events = get_events(ctx.obj, ctx.obj['week'][WEEKDAYS[day]])
+    elif re.search(DATE_PATTERN, day):
+        date = re.search(DATE_PATTERN, day)
+        events = get_events(ctx.obj, dt_from_date(date))
+    else:
+        print('something went wrong with the date')
+        return 1
+
+    save_events(events, schedule)
+    print(f'Saved {schedule_name} to {schedule_name}.json')
+    return 0
 
 if __name__ == '__main__':
     cli()
--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name='gcalendar',
-    description='A calendar tool for Google Calendar',
-    url='#',
+    version='0.1'
+    pymodules=['gcalendar']
+    description='A calendar tool for Google Calendar using python',
     author='iCodeCoolStuff',
     license='ISC',
     install_requires=[
@@ -11,4 +12,8 @@ setup(
         'google-api-python-client', 
         'oauth2client',
     ]
+    entry_points='''
+        [console_scripts]
+        save:cli
+    '''
 )
