a7e680b3e0105ad2f7ad6894090da476a8a95d5c
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2019-01-14 17:28:10 -0500

    some semi-final changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='gcalendar',
     version='1.0',
-    packages=['gcalendar'],
+    py_modules=['gcalendar'],
     description='A command line tool for Google Calendar using python',
     author='iCodeCoolStuff',
     license='MIT',
@@ -15,7 +15,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'gcalendar=gcalendar.gcalendar:cli'
+            'gcalendar=gcalendar:cli'
         ],
     },
 )
